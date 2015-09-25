f361e6f8a56dd03a021a0775020672c4a06ab2f3
Dan Gal <dangalg@gmail.com>
2015-09-25 15:00:09 +0300

    Added sword swing animation Added enemy knight

---
+++ Assets/UnitAttack.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UnitAttack : MonoBehaviour {
+
+	public bool attack;
+
+	void OnTriggerEnter(Collider other) {
+		if (other.gameObject.tag != gameObject.tag) {
+			AttackEnemy(other);
+		}
+	}
+
+	void AttackEnemy(Collider other){
+		attack = true;
+		MoveTowardsEnemyPosition ();
+	}
+
+	void MoveTowardsEnemyPosition(){
+		
+	}
+
+	// Applies an upwards force to all rigidbodies that enter the trigger.
+	void OnTriggerStay(Collider other) {
+		if (other.attachedRigidbody)
+			other.attachedRigidbody.AddForce(Vector3.up * 10);        
+	}
+
+}
